e647d182e8891c756ec31783657b40a949fcabe0
Reuben Frankel <rfrankel@matatika.com>
2022-06-06 16:42:00 +0100

    Remove `MarkupSafe` reinstall temporary fix

--- bundle/orchestrate/tap-trello/elt.sh
+++ bundle/orchestrate/tap-trello/elt.sh
@@ -3,10 +3,6 @@ meltano install loader "$LOADER"
 meltano install transform "$EXTRACTOR"
 meltano install transformer dbt
 
-# Temporary fix for markdown dependencies issue: https://github.com/dbt-labs/dbt-core/issues/4745
-.meltano/transformers/dbt/venv/bin/pip3 install --force-reinstall MarkupSafe==2.0.1
-
-
 # install dbt deps
 meltano invoke dbt deps
 
